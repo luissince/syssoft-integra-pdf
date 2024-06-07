78d40a5af2915bae317fe65677950012851ec991
Luis Lara <xanderls25@icloud.com>
2024-06-06 19:58:46 -0500

    Creando pdf de pre venta

--- controller/venta.py
+++ controller/venta.py
@@ -32,6 +32,29 @@ async def generar_pdf_ticket(venta: VentaGeneral.Body):
         print(ex)
         # Manejar errores generales
         return response_custom_error(message="Error de servidor: "+str(ex), code=500)
+    
+
+@routerVenta.post('/pre/ticket', tags=[tag])
+async def generar_pdf_ticket(venta: VentaGeneral.Body):
+    try:
+        data_html = VentaGeneral.run(venta)
+
+        count = len(venta.plazos) + len(venta.ventaDetalle)
+
+        # Generar PDF
+        pdf_in_memory = generar_ticket(
+            path_template='templates/venta',
+            name_html='pre_ticket.html',
+            data=data_html,
+            count=count,
+            height= 8.5)
+
+        # Devolver el PDF como respuesta
+        return response_custom_pdf(data=pdf_in_memory.getvalue(), file_name="file_pre_tiket_venta.pdf")
+    except Exception as ex:
+        print(ex)
+        # Manejar errores generales
+        return response_custom_error(message="Error de servidor: "+str(ex), code=500)
 
 
 @routerVenta.post('/a4', tags=[tag])
@@ -51,6 +74,23 @@ async def generar_pdf_a4(venta: VentaGeneral.Body):
         # Manejar errores generales
         return response_custom_error(message="Error de servidor: "+str(ex), code=500)
 
+@routerVenta.post('/pre/a4', tags=[tag])
+async def generar_pdf_a4(venta: VentaGeneral.Body):
+    try:
+        data_html = VentaGeneral.run(venta)
+
+        # Generar PDF
+        pdf_in_memory = generar_a4(
+            path_template='templates/venta',
+            name_html='pre_a4.html',
+            data=data_html)
+
+        # Devolver el PDF como respuesta
+        return response_custom_pdf(data=pdf_in_memory.getvalue(), file_name="file_a4_venta.pdf")
+    except Exception as ex:
+        # Manejar errores generales
+        return response_custom_error(message="Error de servidor: "+str(ex), code=500)
+    
 @routerVenta.post('/reporte/a4', tags=[tag])
 async def generar_pdf_a4(venta: VentaReporte.Body):
     try:
